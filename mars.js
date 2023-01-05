6cab40882b0e52656a7f6d227588527771394886
Typonomy <lqconnor@alaska.edu>
2023-01-05 07:52:31 -0500

    Add files via upload

---
+++ mars.js
@@ -0,0 +1,2 @@
+document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Typonomy/RealTwitterBlue-MarsEdition/main/mars.jpg')";
+document.body.style.backgroundAttachment = "fixed";
