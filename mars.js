fec877a72f70e9e4a124528a1a73f4736335c158
Typonomy <lqconnor@alaska.edu>
2023-01-05 08:07:49 -0500

    Update mars.js

--- mars.js
+++ mars.js
@@ -1,2 +1,2 @@
-document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Typonomy/RealTwitterBlue-MarsEdition/main/mars2.jpg')";
+document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Typonomy/RealTwitterBlue-MarsEdition/main/mars3.jpg')";
 document.body.style.backgroundAttachment = "fixed";
